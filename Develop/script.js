8cf21f1d65df872c87ad79f2c68b66049248e71b
Michael Li <dev.mike011235@gmail.com>
2020-06-13 00:05:16 -0400

    Added colorOfBackground() function. Tested, working.

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,9 @@
 //TEST AREA///////////////////////////
 todaysDate();
-//generateRow(10, "PM");
 generateHoursList();
-// pastPresentFutureColor(9);
-// pastPresentFutureColor(10);
-// pastPresentFutureColor(11);
-// pastPresentFutureColor(12);
-// pastPresentFutureColor(1);
-// pastPresentFutureColor(2);
-// pastPresentFutureColor(3);
-// pastPresentFutureColor(4);
-// pastPresentFutureColor(5);
-// colorOfBackground(9);
-// colorOfBackground(10);
-// colorOfBackground(11);
-// colorOfBackground(12);
-// colorOfBackground(1);
-// colorOfBackground(2);
-// colorOfBackground(3);
-// colorOfBackground(4);
-// colorOfBackground(5);
+colorOfBackground();
+//generateRow(10, "PM");
 //TEST AREA///////////////////////////
-
 
 // Generates a new HTML <div>.
 // The div is populated with other HTML tags.
@@ -46,10 +28,10 @@ function generateRow(hour, meridiem) {
     taskButton.addClass("user-text-input-button");
     taskButton.attr("type", "button");
     taskButton.text("Add Event");
-    taskButton.attr("id", hour+meridiem+"-button");
+    taskButton.attr("id", hour+meridiem+"-button"); // Used for onclick listener.
     var inputBox = $("<input>").addClass("form-control");
     inputBox.addClass("user-text-input-field");
-    inputBox.attr("id", hour+meridiem);
+    inputBox.attr("id", hour); // Used to select element and apply color based on time.
     inputBox.attr("type", "text");
     inputBox.attr("aria-label", "Sizing example input");
     inputBox.attr("aria-describedby", "inputGroup-sizing-default");
@@ -75,33 +57,81 @@ function generateHoursList() {
     var workHours = [9,10,11,12,1,2,3,4,5];
     for (var i = 0; i < workHours.length; i++) {
         var meridiem = "";
-        if (i < 4) {
+        if (i < 3) {
             meridiem = "AM";
-        } else if (i >= 4) {
+        } else if (i >= 3) {
             meridiem = "PM";
         }
         generateRow(workHours[i], meridiem);
     }
 }
 
+// Colors input box background based on past, present, and future. 
+function colorOfBackground() {
+    var currentHour = parseInt(moment().format("H"));
+    var currentHoursWeight;
+    var hoursAndTheirWeights = [[9,1],[10,2],[11,3],[12,4],[1,5],[2,6],[3,7],[4,8],[5,9]];
+    var invalidMorningHours = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM"];
+    var invalidEveningHours = ["6PM", "7PM", "8PM","9PM", "10PM", "11PM"];
+    var currentHourAndMeridiem = moment().format("hA");
+
+// Gets weight of the current hour
+// loop through array again
+//      if currentHoursWeight > than value we are looking at, color background grey
+//      if currentHoursWeight == than value we are looking at, color background red
+//      if currentHoursWeight < than value we are looking at, color background green
+//      (the input field has an id="9-5", ie, id="9", refer to line 53 in this file)
+    for (var i = 0; i < hoursAndTheirWeights.length; i++) {
+        if (hoursAndTheirWeights[i][0] === currentHour) {
+            currentHoursWeight = hoursAndTheirWeights[i][1];
+        }
+    }
+
+    for (var i = 0; i < hoursAndTheirWeights.length; i++) {
+        if ((hoursAndTheirWeights[i][1] < currentHoursWeight)) {
+            $("#"+hoursAndTheirWeights[i]).css("background-color", "grey");
+        }
+        if (hoursAndTheirWeights[i][1] == currentHoursWeight) {
+            $("#"+hoursAndTheirWeights[i]).css("background-color", "red");
+        }
+        if (hoursAndTheirWeights[i][1] > currentHoursWeight) {
+            $("#"+hoursAndTheirWeights[i]).css("background-color", "green");
+        }
+    }
+
+// if current hour is in 12am-8am array, color all green
+// if current hour is in 6am-11am array, color all grey
+    if (invalidMorningHours.includes(currentHourAndMeridiem)) {
+        for (var i = 0; i < hoursAndTheirWeights.length; i++) {
+            $("#"+(hoursAndTheirWeights[i])).css("background-color", "green");
+        }
+    }
+    if (invalidEveningHours.includes(currentHourAndMeridiem)) {
+        for (var i = 0; i < hoursAndTheirWeights.length; i++) {
+            $("#"+(hoursAndTheirWeights[i])).css("background-color", "grey");
+        }
+    }
+}
+
+
 // Captures data in field.
 // On "Add Event" button click.
-$(".user-text-input-button").on("click", function(event) {
-    event.preventDefault();
-    var task_buttonClick = $(".user-text-input-field").val();
-    var myObj = {task: task_buttonClick}
+// $(".user-text-input-button").on("click", function(event) {
+//     event.preventDefault();
+//     var task_buttonClick = $(".user-text-input-field").val();
+//     var myObj = {task: task_buttonClick}
 
-    //how do I get this to populate the specific input box on refresh?
+//     //how do I get this to populate the specific input box on refresh?
 
-    var myObj_serialized = JSON.stringify(myObj);
-    console.log(myObj_serialized);
+//     var myObj_serialized = JSON.stringify(myObj);
+//     console.log(myObj_serialized);
 
-    localStorage.setItem("task", myObj_serialized);
-    localStorage.getItem("the_task");
+//     localStorage.setItem("task", myObj_serialized);
+//     localStorage.getItem("the_task");
 
-    var myObj_deserialized = JSON.parse(localStorage.setItem("task", myObj_serialized));
-    console.log(myObj_deserialized);
-});
+//     var myObj_deserialized = JSON.parse(localStorage.setItem("task", myObj_serialized));
+//     console.log(myObj_deserialized);
+// });
 
 // Captures data in field.
 // On "return" press.
@@ -114,94 +144,17 @@ $(".user-text-input-button").on("click", function(event) {
 
 
 
-// function pastPresentFutureColor(hour) { 
-//     var time = hour + "";
-//     time.trim();
-//     var currentMeridiem = moment().format("A");
-//     var current_HourInt = parseInt(moment().format("h"));
-//     var arr = [1,2,3,4,5];
-
-//     // console.log(time);
-//     // console.log(typeof time);
-//     // console.log(moment().format("h"));
-//     // console.log(typeof moment().format("h"));
-
-//     // console.log("boom");
-//     // console.log(parseInt(time));
-//     // console.log(typeof parseInt(time));
-//     // console.log(typeof parseInt(moment().format("h")));
-
-//     if (time === moment().format("h")) {
-//         $(("#"+time+currentMeridiem)).css("background-color", "red");
-//     }
-    
-//     if ((arr.includes(hour)) && (arr.includes(current_HourInt)) && (hour > current_HourInt)) {
-//         $(("#"+time+currentMeridiem)).css("background-color", "green");
-//         console.log("AAAAA");
-//     } else if ((hour < current_HourInt)) {
-//         $(("#"+time+currentMeridiem)).css("background-color", "grey");
-//     } else if (hour > current_HourInt) {
-//         $(("#"+time+currentMeridiem)).css("background-color", "green");
-//     }
-// }
-
-// function colorOfBackground(hour) {
-//     var amTimes = [9,10,11,12];
-//     var pmTimes = [1,2,3,4,5];
-//     var currentMeridiem = moment().format("A"); //string
-//     var currentHour = moment().format("h"); //string
-//     var current_hourInt = parseInt(moment().format("h")); //int 
-//     console.log(moment("13:00", 'HH:mm').format('hh:mm a'));
-//     console.log("BOOM");
-//     console.log(hour);
-//     if (amTimes.includes(hour)) {
-//         console.log("test 1");
-//         if (hour < current_hourInt) {
-//             $(("#"+hour+currentMeridiem)).css("background-color", "grey");
-//             console.log("test 2");
-//         } else if (hour === current_hourInt) {
-//             $(("#"+hour+currentMeridiem)).css("background-color", "red");
-//             console.log("test 3");
-//         } else if (hour > current_hourInt) {
-//             $(("#"+hour+currentMeridiem)).css("background-color", "green");
-//             console.log("test 3");
-//         }
-//     }
-//     console.log("BABY");
-
-// }
-
-// function colorOfBackground(hour) {
-//     var arrHoursMeridiem = [9,10,11,12,1,2,3,4,5];
-//     var arrHoursMilitary = [9,10,11,12,13,14,15,16,17];
-//     var currentMeridiem = moment().format("A"); //string
-//     var currentHour = moment().format("h"); //string
-//     var current_meridiemHour = parseInt(moment().format("h")); //int 
-//     var current_militaryHour = parseInt(moment().format("H"));
-//     console.log(current_militaryHour);
-
-//     if (hour  === current_meridiemHour) {
-//         $(("#"+hour+currentMeridiem)).css("background-color", "red");
-//     } else {
-//         for (var i = 0; i < arrHoursMeridiem.length; i++) {
-//             if (current_meridiemHour === arrHoursMeridiem[i]) {
-//                 for (var j = i; j < arrHoursMilitary.length; i++) {
-//                     $(("#"+hour+currentMeridiem)).css("background-color", "grey");
-//                 }
-//             } else if (current_meridiemHour === arrHoursMeridiem[i]) {
-//                 for (var j = 0; j < arrHoursMilitary[i]; i++) {
-//                     $(("#"+hour+currentMeridiem)).css("background-color", "green");
-//                 }
-//             }
-//         } 
-//     }
-// }
 
 
 
-/* 
-1) ask phil about token failed to consume. 
-2) Don't know why the keypress listener will take the value stored in a var inside something else's local scope. 
-3) Can;t get background color code to work. Logic help.
-4) I don't know what strategy to use to record user input, store it, and output it upon refresh. 
-*/
+
+
+
+//////////QUESTIONS FOR PHIL////////////////
+// 1) ask phil about token failed to consume. 
+// 2) Don't know why the keypress listener will take the value stored in a var inside something else's local scope. 
+// 3) Can;t get background color code to work. Logic help.
+// 4) I don't know what strategy to use to record user input, store it, and output it upon refresh. 
+// 5) Can you compare keys to some other value? ie. var x = {1:one}, does key = 1 evaluate to true?
+        // ended up using an array. I don't fundamentally understand objects, i guess. Not good for iterating with? I seems like Objects are useful if you already know the key, but not if you are trying to find the key. Is this correct?
+//////////QUESTIONS FOR PHIL////////////////
